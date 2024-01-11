2e6bf9c52b09080f284817dcbd4c51a6db6b7b3c
AndyyDestiny <134987155+AndyyDestiny@users.noreply.github.com>
2024-01-10 19:03:29 -0500

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import sys
+
+sys.stdin = open("traffic.in", "r")
+sys.stdout = open("traffic.out", "w")
+
 b = int(input())
+
 info = []
 status = []
 ranges = []
@@ -9,29 +15,44 @@ for i in range(b):
     ranges.append([eval(i) for i in info[i][1:3]])
 
 x, y = 0, 0
-x2, y2 = -1, -1
+x2, y2 = -69, -69
 
 for i in range(len(ranges)):
-    if x2 != -1 and status[i] == "on":
-        x2 += ranges[i][0]
-        y2 += ranges[i][1]
-    if x2 != -1 and status[i] == "off":
-        x2 -= ranges[i][1]
-        y2 -= ranges[i][0]
     if status[i] == "none":
         if ranges[i][0] > x2:
             x2 = ranges[i][0]
-        if ranges[i][1] < y2 or y2 == -1:
+        if ranges[i][1] < y2 or y2 == -69:
             y2 = ranges[i][1]
+    if x2 != -69 and status[i] == "on":
+        x2 += ranges[i][0]
+        y2 += ranges[i][1]
+    if x2 != -69 and status[i] == "off":
+        x2 -= ranges[i][1]
+        y2 -= ranges[i][0]
+if x2 < 0:
+    x2 = 0
+if y2 < 0:
+    y2 = 0
 
-print(f"ENDING CARS: {x2}, {y2}")
+l = [x2, y2]
 
 for i in range(len(ranges)):
-    if x2 != -1 and status[i] == "on":
-        x2 -= ranges[i][0]
-        y2 -= ranges[i][1]
-    if x2 != -1 and status[i] == "off":
-        x2 += ranges[i][1]
-        y2 += ranges[i][0]
+    i = len(ranges) - i - 1
+    if status[i] == "none":
+        if ranges[i][0] > x2:
+            x2 = ranges[i][0]
+        if ranges[i][1] < y2:
+            y2 = ranges[i][1]
+    if status[i] == "off":
+        x2 += ranges[i][0]
+        y2 += ranges[i][1]
+    if status[i] == "on":
+        x2 -= ranges[i][1]
+        y2 -= ranges[i][0]
+if x2 < 0:
+    x2 = 0
+if y2 < 0:
+    y2 = 0
 
-print(f"STARTING CARS: {x2}, {y2}")
+print(x2, y2)
+print(l[0], l[1])
